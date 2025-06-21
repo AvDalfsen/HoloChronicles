ffe1741f1b6a03483f827f4846ac57b721542806
AvDalfsen <42408840+AvDalfsen@users.noreply.github.com>
2025-06-21 19:26:30 +0200

    Forgot to clean this up..

--- HoloChronicles.server/Services/XMLParsers/SourceParser.cs
+++ HoloChronicles.server/Services/XMLParsers/SourceParser.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml;
-
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 
 namespace HoloChronicles.Server.Services.XMLParsers
 {
@@ -22,69 +20,6 @@ namespace HoloChronicles.Server.Services.XMLParsers
                 .Where(s => !string.IsNullOrWhiteSpace(s))
                 .Cast<string>()
                 .ToList();
-
-            return sources;
-        }
-    }
-}
-
-
-namespace HoloChronicles.Server.Services.XMLParsers
-{
-    public class SourceParser
-    {
-        public static List<string> ParseSources(XmlElement element)
-        {
-            var sources = new List<string>();
-
-            XmlNodeList sourcesNodeList = element.GetElementsByTagName("Sources");
-            if (sourcesNodeList.Count > 0)
-            {
-                XmlElement? sourcesNode = sourcesNodeList[0] as XmlElement;
-                if (sourcesNode != null)
-                {
-                    XmlNodeList sourceNodes = sourcesNode.GetElementsByTagName("Source");
-
-                    foreach (XmlNode sourceNode in sourceNodes)
-                    {
-                        if (sourceNode is XmlElement sourceElement)
-                        {
-                            string page = sourceElement.GetAttribute("Page");
-
-                            if (!string.IsNullOrEmpty(page))
-                            {
-                                sources.Add($"{sourceElement.InnerText} - p. {page}");
-                            }
-                            else
-                            {
-                                sources.Add(sourceElement.InnerText);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    // If <Sources> tag is missing, look for individual <Source> tag
-                    XmlNodeList sourceNodes = element.GetElementsByTagName("Source");
-
-                    foreach (XmlNode sourceNode in sourceNodes)
-                    {
-                        if (sourceNode is XmlElement sourceElement)
-                        {
-                            string page = sourceElement.GetAttribute("Page");
-
-                            if (!string.IsNullOrEmpty(page))
-                            {
-                                sources.Add($"{sourceElement.InnerText} - p. {page}");
-                            }
-                            else
-                            {
-                                sources.Add(sourceElement.InnerText);
-                            }
-                        }
-                    }
-                }
-            }
 
             return sources;
         }
